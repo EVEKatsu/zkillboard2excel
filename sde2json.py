83f75d6450d69e0e7c05c52af0a650307fcfec6e
Omochin <omochin7@gmail.com>
2019-04-01 23:06:53 +0900

    first commit

---
+++ sde2json.py
@@ -0,0 +1,315 @@
+# v1.0.0
+import os
+import time
+import json
+import copy
+import zipfile
+import urllib.error
+import urllib.request
+import urllib.parse
+from collections import OrderedDict
+
+# pip install -r requirements.txt
+import yaml
+from bs4 import BeautifulSoup
+
+LANGUAGES = [
+    'de',
+    'en',
+    'fr',
+    'ja',
+    'ru',
+    'zh',
+]
+
+TYPES_JSON_PATH = os.path.join('.', 'types.json')
+
+UNIVERSES_JSON_PATH = os.path.join('.', 'universes.json')
+UNIVERSE_IDS = OrderedDict(
+    eve=1,
+    wormhole=2,
+    abyssal=3,
+    penalty=4,
+)
+DEFAULT_UNIVERSES = OrderedDict()
+DEFAULT_UNIVERSES['1'] = OrderedDict(
+    name='eve',
+    de='eve',
+    en='eve',
+    fr='eve',
+    ja='\u30cb\u30e5\u30fc\u30a8\u30c7\u30f3',
+    ru='eve',
+    zh='eve',
+)
+DEFAULT_UNIVERSES['2'] = OrderedDict(
+    name='wormhole',
+    de='wormhole',
+    en='wormhole',
+    fr='wormhole',
+    ja='\u30ef\u30fc\u30e0\u30db\u30fc\u30eb',
+    ru='wormhole',
+    zh='wormhole',
+)
+DEFAULT_UNIVERSES['3'] = OrderedDict(
+    name='abyssal',
+    de='abyssal',
+    en='abyssal',
+    fr='abyssal',
+    ja='\u30a2\u30d3\u30b5\u30eb',
+    ru='abyssal',
+    zh='abyssal',
+)
+DEFAULT_UNIVERSES['4'] = OrderedDict(
+    name='penalty',
+    de='penalty',
+    en='penalty',
+    fr='penalty',
+    ja='\u30da\u30ca\u30eb\u30c6\u30a3',
+    ru='penalty',
+    zh='penalty',
+)
+
+def get_json_by_file(path):
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            return json.load(file)
+    return {}
+
+def get_json_by_url(url):
+    while(True):
+        try:
+            print('Download: ' + url)
+            with urllib.request.urlopen(url) as html:
+                data = json.loads(html.read().decode())
+            time.sleep(2)
+            return data
+        except urllib.error.HTTPError:
+            print('urllib.error.HTTPError: ' + url)
+            time.sleep(60)
+
+def get_supported_names(names):
+    lang_dict = OrderedDict()
+    lang_dict['name'] = names['en']
+    for lang in LANGUAGES:
+        if lang in names:
+            lang_dict[lang] = names[lang]
+        else:
+            lang_dict[lang] = names['en']
+    return lang_dict
+
+def get_supported_names_by_esi(target_type, target_id, default_name):
+    names = {'en': default_name}
+
+    for lang in LANGUAGES:
+        if lang == 'en':
+            continue
+
+        esi_url = 'https://esi.evetech.net/latest/universe/%s/%d?language=%s' % (
+            target_type,
+            target_id,
+            lang,
+        )
+
+        names[lang] = get_json_by_url(esi_url)['name']
+    return get_supported_names(names)
+
+def generate_types_json(version):
+    print('Create: ' + TYPES_JSON_PATH)
+
+    # # Only categories to which the killmail is issued.
+    # include_category_ids = [
+    #     6,  # Ship
+    #     18, # Drone
+    #     22, # Deployable
+    #     23, # Starbase
+    #     46, # Orbitals
+    #     65, # Structure
+    #     87, # Fighter
+    # ]
+
+    include_category_ids = [
+        4,  # Material
+        6,  # Ship
+        7,  # Module
+        8,  # Charge
+        9,  # Blueprint
+        16, # Skill
+        18, # Drone
+        20, # Implant
+        22, # Deployable
+        23, # Starbase
+        25, # Asteroid
+        30, # Apparel
+        32, # Subsystem
+        42, # Planetary Resources
+        43, # Planetary Commodities
+        46, # Orbitals
+        65, # Structure
+        66, # Structure Module
+        87, # Fighter
+        91, # Super Kerr-Induced Nanocoatings
+    ]
+
+    include_group_ids = []
+    base_path = os.path.join('.', 'sde', 'fsd')
+
+    types = OrderedDict()
+    types['version'] = version
+    types['categories'] = OrderedDict()
+
+    with open(os.path.join(base_path, 'categoryIDs.yaml')) as file:
+        for i, items in yaml.load(file, Loader=yaml.FullLoader).items():
+            if i not in include_category_ids:
+                continue
+
+            types['categories'][i] = get_supported_names(items['name'])
+
+    types['groups'] = OrderedDict()
+    with open(os.path.join(base_path, 'groupIDs.yaml')) as file:
+        for i, items in yaml.load(file, Loader=yaml.FullLoader).items():
+            if items['categoryID'] not in include_category_ids or not items['name']:
+                continue
+
+            include_group_ids.append(i)
+            types['groups'][i] = get_supported_names(items['name'])
+            types['groups'][i]['category_id'] = items['categoryID']
+
+    types['types'] = {}
+    with open(os.path.join(base_path, 'typeIDs.yaml')) as file:
+        for i, items in yaml.load(file, Loader=yaml.FullLoader).items():
+            if items['groupID'] not in include_group_ids or not items['name']:
+                continue
+
+            types['types'][i] = get_supported_names(items['name'])
+            types['types'][i]['group_id'] = items['groupID']
+
+    with open(TYPES_JSON_PATH, 'w', encoding='utf-8') as file:
+        json.dump(types, file, indent=4)
+
+def generate_universes_json(version):
+    print('Create: ' + UNIVERSES_JSON_PATH)
+
+    level_items = [
+        # level_name, level_id_name, level_filename, parent_level_id_name, level_included_keys
+        ('regions', 'regionID', 'region.staticdata', 'universe_id', []),
+        ('constellations', 'constellationID', 'constellation.staticdata', 'region_id', []),
+        ('systems', 'solarSystemID', 'solarsystem.staticdata', 'constellation_id', ['security']),
+    ]
+
+    universes = OrderedDict(
+        version=version,
+        universes=OrderedDict(),
+        regions=OrderedDict(),
+        constellations=OrderedDict(),
+        systems=OrderedDict(),
+    )
+
+    universes['universes'] = copy.deepcopy(DEFAULT_UNIVERSES)
+
+    cached_universes = get_json_by_file(UNIVERSES_JSON_PATH)
+    if not cached_universes:
+        cached_universes = {
+            'regions': {},
+            'constellations': {},
+            'systems': {},
+        }
+
+    def get_universe_values(path, *names):
+        values = {}
+        with open(path) as file:
+            target_yaml = yaml.load(file, Loader=yaml.FullLoader)
+            for name in names:
+                values[name] = target_yaml[name]
+        return values
+
+    def recursive(nest, parent_universe_id, path):
+        for name in os.listdir(path):
+            next_path = os.path.join(path, name)
+
+            if not os.path.isdir(next_path):
+                continue
+
+            level_name, level_id_name, level_filename, parent_level_id_name, level_included_keys = level_items[nest]
+            values = get_universe_values(os.path.join(next_path, level_filename), level_id_name, *level_included_keys)
+
+            universe_id = values[level_id_name]
+            universe_id_str = str(universe_id)
+
+            if universe_id_str in cached_universes[level_name]:
+                print('Cached: ' + next_path)
+                universes[level_name][universe_id_str] = cached_universes[level_name][universe_id_str]
+            else:
+                print('Add: ' + next_path)
+                universes[level_name][universe_id_str] = get_supported_names_by_esi(
+                    level_name,
+                    universe_id,
+                    name,
+                )
+                universes[level_name][universe_id_str][parent_level_id_name] = parent_universe_id
+
+                for key in level_included_keys:
+                    universes[level_name][universe_id_str][key] = values[key]
+
+            if nest <= -1:
+                recursive(nest + 1, universe_id, next_path)
+
+    base_path = os.path.join('.', 'sde', 'fsd', 'universe')
+    for universe_name in os.listdir(base_path):
+        recursive_path = os.path.join(base_path, universe_name)
+        if os.path.isdir(recursive_path):
+            recursive(0, UNIVERSE_IDS[universe_name], recursive_path)
+
+    with open(UNIVERSES_JSON_PATH, 'w', encoding='utf-8') as file:
+        json.dump(universes, file, indent=4)
+
+def update_version(old_version):
+    resources_url = 'https://developers.eveonline.com/resource/resources'
+
+    while(True):
+        try:
+            with urllib.request.urlopen(resources_url) as html:
+                soup = BeautifulSoup(html, 'html.parser')
+            break
+        except urllib.error.HTTPError:
+            print('urllib.error.HTTPError: ' + resources_url)
+
+    for link in soup.find('div', attrs={'class': 'content'}).findAll('a'):
+        sde_url = link['href']
+
+        if 'https://cdn1.eveonline.com/data/sde/tranquility/' in sde_url:
+            parsed = urllib.parse.urlparse(sde_url)
+            sde_filename = parsed.path[1:].split('/')[-1]
+
+            version, ext = os.path.splitext(sde_filename)
+            if ext == '.zip' and 'sde-' in version and '-TRANQUILITY' in version:
+                if version == old_version:
+                    print('Cached: ' + sde_url)
+                    break
+
+                while(True):
+                    try:
+                        print('Download: ' + sde_url)
+                        with urllib.request.urlopen(sde_url) as data:
+                            with open('sde.zip', mode='wb') as file:
+                                file.write(data.read())
+                        break
+                    except urllib.error.HTTPError:
+                        print('urllib.error.HTTPError: ' + sde_url)
+
+                with zipfile.ZipFile('sde.zip') as zfile:
+                    zfile.extractall()
+    return version
+
+def main():
+    old_version = None
+    if os.path.isfile(TYPES_JSON_PATH):
+        old_version = get_json_by_file(TYPES_JSON_PATH)['version']
+
+    version = update_version(old_version)
+
+    if version != old_version:
+        generate_types_json(version)
+        generate_universes_json(version)
+
+if __name__ == '__main__':
+    main()
---
+++ zkillboard2excel.py
@@ -0,0 +1,437 @@
+import sys
+import os
+import time
+import datetime
+import json
+import csv
+import copy
+import urllib.error
+import urllib.request
+import urllib.parse
+import distutils.util
+from collections import OrderedDict
+
+import openpyxl
+
+import sde2json
+
+
+OPTIONS = [
+    ('--lang', "The language of a export data. Can use 'de', 'en', 'fr', 'ja', 'ru', 'zh'. default: 'en'"),
+    ('--filepath', 'Filepath to export. default: export'),
+    ('--format', "File format to export. Can use 'excel' or 'csv'. default: excel"),
+    ('--clear-cache', 'Finally, clear the cache. default: False'),
+    ('--update-sde', 'Check for SDE Updates. default: False'),
+    ('--page', 'Start page of zKillboard API. default: 1'),
+    ('--limit', 'Number of pages read. default: 1'),
+]
+
+LANGUAGES = [
+    'de',
+    'en',
+    'fr',
+    'ja',
+    'ru',
+    'zh',
+]
+
+TYPES_JSON_PATH = 'types.json'
+
+UNIVERSES_JSON_PATH = 'universes.json'
+
+SETTINGS_JSON_PATH = 'settings.json'
+DEFAULT_SETTINGS = {
+    'zkb_url': '',
+    'lang': 'en',
+    'filepath': 'export',
+    'format': 'excel',
+    'clear-cache': False,
+    'update-sde': False,
+    'page': 1,
+    'limit': 1,
+}
+
+CACHED_JSON_PATH = 'cached.json'
+CACHED_KEYS = (
+    'killmails',
+    'characters',
+    'corporations',
+    'alliances',
+)
+
+LOCALE_MENUITEMS_JSON_PATH = 'locale_menuitems.json'
+MENUITEMS = (
+    'killmail_id',
+    'killmail_time',
+    'ship',
+    'security',
+    'region',
+    'system',
+    'damage',
+    'value',
+    'points',
+    'involved',
+    'character',
+    'corporation',
+    'alliance',
+)
+
+FETCH_MODIFIERS = {
+    'character': 'characterID',
+    'corporation': 'corporationID',
+    'alliance': 'allianceID',
+    'ship': 'shipTypeID',
+    'group': 'groupID',
+    'system': 'solarSystemID',
+    'constellation': 'constellationID',
+    'region': 'regionID',
+}
+
+FOCUS_KEYS = [
+    'character',
+    'corporation',
+    'alliance',
+    'ship',
+    'group',
+]
+
+def get_json_by_file(path):
+    if os.path.isfile(path):
+        with open(path, 'r') as file:
+            return json.load(file)
+    return {}
+
+SETTINGS = copy.deepcopy(DEFAULT_SETTINGS)
+if os.path.isfile(SETTINGS_JSON_PATH):
+    SETTINGS.update(get_json_by_file(SETTINGS_JSON_PATH))
+
+CACHED = get_json_by_file(CACHED_JSON_PATH)
+if not CACHED:
+    for cached_key in CACHED_KEYS:
+        CACHED[cached_key] = {}
+
+TYPES = get_json_by_file(TYPES_JSON_PATH)
+
+UNIVERSES = get_json_by_file(UNIVERSES_JSON_PATH)
+
+LOCALE_MENUITEMS = get_json_by_file(LOCALE_MENUITEMS_JSON_PATH)
+
+def get_json_by_url(url):
+    while(True):
+        try:
+            print('Download: ' + url)
+            with urllib.request.urlopen(url) as html:
+                data = json.loads(html.read().decode())
+            time.sleep(2)
+            return data
+        except urllib.error.HTTPError:
+            print('urllib.error.HTTPError: ' + url)
+            time.sleep(60)
+
+def get_link(target_key, target_id, name):
+    url = 'https://zkillboard.com/%s/%d/' % (target_key, target_id)
+    return '=HYPERLINK("%s", "%s")' % (url, name)
+
+def get_killmail_id(killmail, zkb):
+    return killmail['killmail_id']
+
+def get_killmail_id_for_excel(killmail, zkb):
+    killmail_id = get_killmail_id(killmail, zkb)
+    return get_link('kill', killmail_id, killmail_id)
+
+def get_killmail_time(killmail, zkb):
+    date = datetime.datetime.strptime(killmail['killmail_time'], '%Y-%m-%dT%H:%M:%SZ')
+    return date.strftime('%Y-%m-%d %H:%M')
+
+def get_ship(killmail, zkb):
+    type_id = killmail['ship_id']
+    return TYPES['types'][str(type_id)][SETTINGS['lang']]
+
+def get_ship_for_excel(killmail, zkb):
+    return get_link('ship', killmail['ship_id'], get_ship(killmail, zkb))
+
+def get_security(killmail, zkb):
+    system_id = killmail['system_id']
+    return round(UNIVERSES['systems'][str(system_id)]['security'], 1)
+
+def get_region(killmail, zkb):
+    return UNIVERSES['regions'][str(killmail['region_id'])][SETTINGS['lang']]
+
+def get_region_for_excel(killmail, zkb):
+    return get_link('region', killmail['region_id'], get_region(killmail, zkb))
+
+def get_system(killmail, zkb):
+    system_id = killmail['system_id']
+    return UNIVERSES['systems'][str(system_id)][SETTINGS['lang']]
+
+def get_system_for_excel(killmail, zkb):
+    return get_link('system', killmail['system_id'], get_system(killmail, zkb))
+
+def get_damage(killmail, zkb):
+    return killmail['damage_taken']
+
+def get_value(killmail, zkb):
+    return zkb['totalValue']
+
+def get_value_for_excel(killmail, zkb):
+    return int(get_value(killmail, zkb))
+
+def get_points(killmail, zkb):
+    return zkb['points']
+
+def get_involved(killmail, zkb):
+    return killmail['involved']
+
+def get_player(player_key, player_id):
+    player_id_str = str(player_id)
+    esi_url = 'https://esi.evetech.net/latest/%s/%s/' % (player_key, player_id_str)
+
+    if player_id_str in CACHED[player_key]:
+        print('Cached: ' + esi_url)
+        return CACHED[player_key][player_id_str]
+
+    name = get_json_by_url(esi_url)['name']
+    CACHED[player_key][player_id_str] = name
+    return name 
+
+def get_character(killmail, zkb):
+    name = ''
+    if killmail['character_id']:
+        name = get_player('characters', killmail['character_id'])
+    return name
+
+def get_character_for_excel(killmail, zkb):
+    name = get_character(killmail, zkb)
+    if name:
+        return get_link('character', killmail['character_id'], name)
+    return
+
+def get_corporation(killmail, zkb):
+    name = ''
+    if killmail['corporation_id']:
+        name = get_player('corporations', killmail['corporation_id'])
+    return name
+
+def get_corporation_for_excel(killmail, zkb):
+    name = get_corporation(killmail, zkb)
+    if name:
+        name = get_link('corporation', killmail['corporation_id'], name)
+    return name
+
+def get_alliance(killmail, zkb):
+    name = ''
+    if killmail['alliance_id']:
+        name = get_player('alliances', killmail['alliance_id'])
+    return name
+
+def get_alliance_for_excel(killmail, zkb):
+    name = get_alliance(killmail, zkb)
+    if name:
+        name = get_link('alliance', killmail['alliance_id'], name)
+    return name
+
+def parse_killmail(killmail):
+    ship_id = killmail['victim']['ship_type_id']
+    system_id = killmail['solar_system_id']
+    constellation_id = UNIVERSES['systems'][str(system_id)]['constellation_id']
+    km = {
+        'killmail_id': killmail['killmail_id'],
+        'killmail_time': killmail['killmail_time'],
+        'damage_taken': killmail['victim']['damage_taken'],
+        'involved': len(killmail['attackers']),
+        'ship_id': ship_id,
+        'group_id': TYPES['types'][str(ship_id)]['group_id'],
+        'system_id': system_id,
+        'constellation_id': constellation_id,
+        'region_id':UNIVERSES['constellations'][str(constellation_id)]['region_id'],
+    }
+
+    for player_type in ['character', 'corporation', 'alliance']:
+        player_key = player_type + '_id'
+        if player_key in killmail['victim']:
+            km[player_key] = killmail['victim'][player_key]
+        else:
+            km[player_key] = None
+
+    return km
+
+def get_killmails():
+    killmails = OrderedDict()
+    header = []
+    focus_key = None
+    focus_id = None
+
+    for name in MENUITEMS:
+        header.append(LOCALE_MENUITEMS[name][SETTINGS['lang']])
+
+    parse_result = urllib.parse.urlparse(SETTINGS['zkb_url'])
+    zkb_url = parse_result.scheme + '://' + parse_result.netloc + '/api/'
+
+    for value in parse_result.path.split('/'):
+        if not value:
+            continue
+
+        if value in FETCH_MODIFIERS:
+            zkb_url += FETCH_MODIFIERS[value]
+        else:
+            zkb_url += value
+
+        zkb_url += '/'
+
+        if not focus_id and focus_key:
+            focus_id = int(value)
+
+        if not focus_key and value in FOCUS_KEYS:
+            focus_key = value + '_id'
+
+    for limit in range(SETTINGS['limit']):
+        url = zkb_url + 'page/%d/' % (limit + SETTINGS['page'])
+
+        for zkb in get_json_by_url(url):
+            killmail_id_str = str(zkb['killmail_id'])
+
+            esi_url = 'https://esi.evetech.net/latest/killmails/%d/%s/' % (zkb['killmail_id'], zkb['zkb']['hash'])
+            if killmail_id_str in CACHED['killmails']:
+                killmail = CACHED['killmails'][killmail_id_str]
+            else:
+                killmail = parse_killmail(get_json_by_url(esi_url))
+                CACHED['killmails'][killmail_id_str] = killmail
+
+            values = []
+            for name in MENUITEMS:
+                func_name = 'get_' + name
+
+                if SETTINGS['format'] == 'excel' and func_name + '_for_excel' in globals():
+                    func_name += '_for_excel'
+
+                values.append(globals()[func_name](killmail, zkb['zkb']))
+
+            killmails[zkb['killmail_id']] = values
+
+        save_cache_json()
+
+    return (killmails, header, focus_key, focus_id)
+
+def zkillboard2csv():
+    killmails, header, focus_key, focus_id = get_killmails()
+
+    with open(SETTINGS['filepath'] + '.csv', 'w') as file:
+        writer = csv.writer(file, lineterminator='\n')
+        writer.writerow(header)
+        writer.writerows(killmails.values())
+
+def zkillboard2excel():
+    killmails, header, focus_key, focus_id = get_killmails()
+
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+
+    for i, header_name in enumerate(header):
+        sheet.cell(row=1, column=i+1).value = header_name
+
+    row = 2
+    for killmail_id, killmail in killmails.items():
+        background = '006400'
+        if focus_key and CACHED['killmails'][str(killmail_id)][focus_key] == focus_id:
+            background = '8B0000'
+
+        pattern_fill = openpyxl.styles.PatternFill(patternType='solid', fgColor=background, bgColor=background)
+        border = openpyxl.styles.Border(
+            top=openpyxl.styles.Side(style='thin', color='000000'),
+            bottom=openpyxl.styles.Side(style='thin', color='000000'),
+            left=openpyxl.styles.Side(style='thin', color='000000'),
+            #right=openpyxl.styles.Side(style='thin', color='000000'),
+        )
+
+        for i, value in enumerate(killmail):
+            cell = sheet.cell(row=row, column=i+1)
+            cell.value = value
+            cell.font = openpyxl.styles.fonts.Font(color='FFFFFF')
+            cell.fill = pattern_fill
+            cell.border = border
+
+        for j in range(3):
+            cell = sheet.cell(row=row, column=len(MENUITEMS)+j+1)
+            cell.fill = pattern_fill
+            cell.border = openpyxl.styles.Border(
+                top=openpyxl.styles.Side(style='thin', color='000000'),
+                bottom=openpyxl.styles.Side(style='thin', color='000000'),
+            )
+
+        row += 1
+
+    wb.save(SETTINGS['filepath'] + '.xlsx')
+
+def save_settings_json():
+    with open(SETTINGS_JSON_PATH, 'w', encoding='utf-8') as file:
+        json.dump(SETTINGS, file, indent=4)
+
+def save_cache_json():
+    with open(CACHED_JSON_PATH, 'w', encoding='utf-8') as file:
+        json.dump(CACHED, file)
+
+def clear_cache_json():
+    if os.path.isfile(CACHED_JSON_PATH):
+        os.remove(CACHED_JSON_PATH)
+
+def main():
+    if SETTINGS['format'] == 'excel':
+        zkillboard2excel()
+    else:
+        zkillboard2csv()
+
+    if SETTINGS['clear-cache']:
+        clear_cache_json()
+
+def command_line():
+    if len(sys.argv) < 2:
+        print('''
+usage: python zkillboard2excel.py zKillboard-URL [options]
+Options and arguments:''')
+
+        for option in OPTIONS:
+            print('{0:<15}: {1}'.format(option[0], option[1]))
+        return
+
+    SETTINGS.update(copy.deepcopy(DEFAULT_SETTINGS))
+    SETTINGS['zkb_url'] = sys.argv[1]
+
+    if len(sys.argv) >= 3:
+        for argument in sys.argv[2:]:
+            try:
+                key, value = argument.split('=')
+
+                if key == '--lang':
+                    if value in LANGUAGES:
+                        SETTINGS['lang'] = value
+                    else:
+                        print("Does not support '%s' language" % value)
+                elif key == '--filepath':
+                    SETTINGS['filepath'] = value
+                elif key == '--format':
+                    if value.lower() in ['excel', 'csv']:
+                        SETTINGS['format'] = value.lower()
+                    else:
+                        print("Does not support '%s' format" % value)
+                elif key == '--clear-cache':
+                    SETTINGS['clear-cache'] = bool(distutils.util.strtobool(value))
+                elif key == '--update-sde':
+                    SETTINGS['update-sde'] = bool(distutils.util.strtobool(value))
+                elif key == '--page':
+                    SETTINGS['page'] = int(value)
+                elif key == '--limit':
+                    SETTINGS['limit'] = int(value)
+                else:
+                    print("Option Error: '%s' does not exist" % key)
+            except ValueError:
+                print('Value Error: %s' % argument)
+
+    save_settings_json()
+
+    if SETTINGS['update-sde']:
+        sde2json.main()
+
+    main()
+
+if __name__ == '__main__':
+    command_line()
